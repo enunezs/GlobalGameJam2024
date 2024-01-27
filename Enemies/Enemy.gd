0f5e712bc907ab945475793f67487a87ba613559
Emanuel Nunez <ENunezSardinha@Gmail.com>
2024-01-27 18:41:20 +0000

    Enemy updated

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -72,10 +72,10 @@ func idle():
 		target_time = current_time + prepare_time
 
 func hit_player():
-		if target_time < current_time:
-	target_time = current_time + idle_time
-	state = State.IDLE
-	
+	if target_time < current_time:
+		target_time = current_time + idle_time
+		state = State.IDLE
+		
 
 func prepare_to_attack(): 
 	# Prepare to attack behavior here
